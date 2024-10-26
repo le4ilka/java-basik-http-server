0c8b61127d967a5b2d355beb0ecf0e3f706da383
olga <romanovar-151@ya.ru>
2024-10-26 21:54:35 +0300

    при запуске нужно указать порт

--- src/main/java/ru/le4ilka/http/server/Application.java
+++ src/main/java/ru/le4ilka/http/server/Application.java
@@ -3,6 +3,6 @@ package ru.le4ilka.http.server;
 public class Application {
 
     public static void main(String[] args) {
-        new HttpServer(8188).start();
+        new HttpServer(Integer.parseInt(args[0])).start();
     }
 }
